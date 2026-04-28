ea0fed56b705651eb8a46d3aad9ea7a317cad498
Crist Yang <cr1st4ever@outlook.com>
2026-04-28 21:39:16 +0800

    fix(cmake): 修改cmake配置，编译通过

--- firmware/esp32-s3/test_camera2rgb/main/main.c
+++ firmware/esp32-s3/test_camera2rgb/main/main.c
@@ -4,8 +4,98 @@
 #include "led.h"
 #include "lcd.h"
 #include "myiic.h"
+#include "my_spi.h"
+#include "spilcd.h"
+#include "esp_camera.h"
 #include "xl9555.h"
+#include "spi_sd.h"
+#include "sdmmc_cmd.h"
+#include "jpegd2.h"
+#include "exfuns.h"
 #include <stdio.h>
+
+
+uint8_t *rgb565 = NULL;
+uint8_t sd_check_en = 0;                    /* sd卡检测标志 */
+extern sdmmc_card_t *card;  /* SD / MMC卡结构 */
+camera_fb_t *fb = NULL;
+static const char *TAG = "main";
+/* TASK3 任务 配置
+ * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
+ */
+#define TASK3_PRIO      4                   /* 任务优先级 */
+#define TASK3_STK_SIZE  5*1024              /* 任务堆栈大小 */
+TaskHandle_t            Task3Task_Handler;  /* 任务句柄 */
+void task3(void *pvParameters);             /* 任务函数 */
+SemaphoreHandle_t BinarySemaphore;          /* 二值信号量句柄 */
+
+
+/*
+摄像头引脚配置
+使用 OV2640
+*/
+#define CAM_PIN_PWDN    GPIO_NUM_NC
+#define CAM_PIN_RESET   GPIO_NUM_NC
+#define CAM_PIN_VSYNC   GPIO_NUM_47
+#define CAM_PIN_HREF    GPIO_NUM_48
+#define CAM_PIN_PCLK    GPIO_NUM_45
+#define CAM_PIN_XCLK    GPIO_NUM_NC
+#define CAM_PIN_SIOD    GPIO_NUM_39
+#define CAM_PIN_SIOC    GPIO_NUM_38
+#define CAM_PIN_D0      GPIO_NUM_4
+#define CAM_PIN_D1      GPIO_NUM_5
+#define CAM_PIN_D2      GPIO_NUM_6
+#define CAM_PIN_D3      GPIO_NUM_7
+#define CAM_PIN_D4      GPIO_NUM_15
+#define CAM_PIN_D5      GPIO_NUM_16
+#define CAM_PIN_D6      GPIO_NUM_17
+#define CAM_PIN_D7      GPIO_NUM_18
+
+
+#define CAM_PWDN(x)         do{ x ? \
+                                (xl9555_pin_write(OV_PWDN_IO, 1)):       \
+                                (xl9555_pin_write(OV_PWDN_IO, 0));       \
+                            }while(0)
+
+#define CAM_RST(x)          do{ x ? \
+                                (xl9555_pin_write(OV_RESET_IO, 1)):       \
+                                (xl9555_pin_write(OV_RESET_IO, 0));       \
+                            }while(0)
+
+/* 摄像头配置 */
+static camera_config_t camera_config = {
+    /* 引脚配置 */
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SIOD,
+    .pin_sccb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    /* XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental) */
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG,     /* YUV422,GRAYSCALE,RGB565,JPEG */
+    .frame_size = FRAMESIZE_QVGA,       /* QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates */
+
+    .jpeg_quality = 12,                 /* 0-63, for OV series camera sensors, lower number means higher quality */
+    .fb_count = 2,                      /* When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode */
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
 
 
 /**
